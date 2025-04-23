6c4a8c12b108011896ba81318d5208567eb624e4
Esn Microcredit <esnmicrocredit@Esns-MacBook-Pro.local>
2025-04-23 12:34:02 +0300

    first commit

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-center text-blue-500 text-2xl">Dashboard Page</h1>
+    </div>
+  )
+}
